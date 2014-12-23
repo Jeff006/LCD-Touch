f1ae132624b1d5f6bd73d5005c01997ef57630d2
Jeff006 <leguen.jf@gmail.com>
2014-12-23 09:24:32 +0100

    Initial LCD touch project
    Coocox project for STM32F4 discovery (STM32F407VG) with LCD connected
    (SSD2119) and STMPE811 Touch screen controller. Running example

---
+++ lcd_log_conf.h
@@ -0,0 +1,18 @@
+#ifndef  __LCD_LOG_CONF_H__
+#define  __LCD_LOG_CONF_H__
+
+/* Comment the line below to disable the scroll back and forward features */
+#define LCD_SCROLL_ENABLED
+/* Define the LCD default text color */
+#define LCD_LOG_DEFAULT_COLOR    LCD_COLOR_WHITE
+/* Define the display window settings */
+#define YWINDOW_MIN              3
+#define YWINDOW_SIZE             12
+#define XWINDOW_MAX              50
+/* Define the cache depth */
+#define CACHE_SIZE               50
+
+#endif
+
+
+
